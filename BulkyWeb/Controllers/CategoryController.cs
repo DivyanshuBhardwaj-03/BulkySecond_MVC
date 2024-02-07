5545a8a597545bd9db6ddc5452fb841e1bca4fd3
Divyanshu Bhardwaj <78968568+DivyanshuBhardwaj-03@users.noreply.github.com>
2024-02-07 20:49:45 +0530

    Server Side, Custom and Client Side Validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -24,9 +24,17 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "Category Name cannot be same as Display Order");
+            //}
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+            
+            return View();
         }
     }
 }
