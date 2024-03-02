ce28ac39a95db4feedb1309c55f651e33e5be85e
Divyanshu Bhardwaj <78968568+DivyanshuBhardwaj-03@users.noreply.github.com>
2024-03-02 12:33:32 +0530

    product.js file error corrected as per TA's instructions.

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -14,7 +14,7 @@ function loadDataTable() {
             {
                 data: 'id',
                 "render": function (data) {
-                    return '<div class="w-75 btn-group" role="group"><a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2" <i class="bi bi-pencil-fill" ></i> Edit </a> <a class="btn btn-danger mx-2" > <i class="bi bi-trash-fill"></i>Delete</a></div > '
+                    return '<div class="w-75 btn-group" role="group">< a href = "/admin/product/upsert?id=${data}" class="btn btn-primary mx-2" > <i class="bi bi-pencil-fill" ></i> Edit </a ><a class="btn btn-danger mx-2" > <i class="bi bi-trash-fill"></i>Delete</a></div > '
 
                 },
 
